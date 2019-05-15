3de7c5a0971f12e485dbaeb053c06ce726e6fcf6
fverdugo <fverdugo@cimne.upc.edu>
2019-05-15 17:38:09 +0200

    Extending creation of systems of polynomials given by FixedPolynomials

--- src/TensorPolynomialBases.jl
+++ src/TensorPolynomialBases.jl
@@ -1,5 +1,31 @@
 module TensorPolynomialBases
 
-greet() = print("Hello World!")
+using FixedPolynomials
+using DynamicPolynomials: @polyvar
+
+import FixedPolynomials: System
+
+function (::Type{System{T}})(filter::Function, order::Int, dim::Int) where T
+  B = _setup_system(filter,order,dim,T)
+end
+
+function _setup_system(filter,O,D,T)
+  @polyvar x[1:D]
+  o = CartesianIndex(ones(Int,D)...)
+  cs = CartesianIndices( tuple(fill(O+1,D)...) )
+  A = Polynomial{T}[]
+  for c in cs
+    e = Tuple(c-o)
+    if filter(e,O)
+      f = 1
+      for (xi,p) in zip(x,e)
+        f *= xi^p
+      end
+      poly = Polynomial{Float64}(f)
+      push!(A,f)
+    end
+  end
+  System(A)
+end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,21 @@
+module TensorPolynomialBasesTests
+
 using TensorPolynomialBases
 using Test
+using FixedPolynomials
 
-@testset "TensorPolynomialBases.jl" begin
-    # Write your own tests here.
-end
+filter(e,O) = (sum(e) <= O)
+
+order = 3
+dim = 2
+B = System{Float64}(filter,order,dim)
+
+cfg = JacobianConfig(B)
+
+x = [1.0,2.0]
+
+@test evaluate(B,x,cfg) ≈ [1.0, 1.0, 1.0, 1.0, 2.0, 2.0, 2.0, 4.0, 4.0, 8.0]
+@test jacobian(B,x,cfg) ≈ [0.0 0.0; 1.0 0.0; 2.0 0.0; 3.0 0.0; 0.0 1.0;
+                     2.0 1.0; 4.0 1.0; 0.0 4.0; 4.0 4.0; 0.0 12.0]
+
+end # module TensorPolynomialBasesTests
