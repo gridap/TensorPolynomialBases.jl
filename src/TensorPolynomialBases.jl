66a2a6c66696f3c92c89918f3808a45952ee55bb
Francesc Verdugo <fverdugo@cimne.upc.edu>
2019-05-16 09:24:24 +0200

    Starting to add FixedPolynomialBasis

--- src/TensorPolynomialBases.jl
+++ src/TensorPolynomialBases.jl
@@ -1,11 +1,15 @@
 module TensorPolynomialBases
 
-using FixedPolynomials
 using DynamicPolynomials: @polyvar
+import FixedPolynomials; const fp = FixedPolynomials
+using StaticArrays
 
-import FixedPolynomials: System
+export FixedPolynomialBasis, ScratchData, gradient_type
 
-function (::Type{System{T}})(filter::Function, order::Int, dim::Int) where T
+"""
+Create a FixedPolynomials.System object representing a monomial basis. 
+"""
+function (::Type{fp.System{T}})(filter::Function, order::Int, dim::Int) where T
   B = _setup_system(filter,order,dim,T)
 end
 
@@ -13,7 +17,7 @@ function _setup_system(filter,O,D,T)
   @polyvar x[1:D]
   o = CartesianIndex(ones(Int,D)...)
   cs = CartesianIndices( tuple(fill(O+1,D)...) )
-  A = Polynomial{T}[]
+  A = fp.Polynomial{T}[]
   for c in cs
     e = Tuple(c-o)
     if filter(e,O)
@@ -21,11 +25,114 @@ function _setup_system(filter,O,D,T)
       for (xi,p) in zip(x,e)
         f *= xi^p
       end
-      poly = Polynomial{Float64}(f)
-      push!(A,f)
+      poly = fp.Polynomial{T}(f)
+      push!(A,poly)
     end
   end
-  System(A)
+  fp.System(A)
+end
+
+"""
+Scratch data associated with FixedPolynomialBasis
+"""
+struct FixedScratchData{T,C<:fp.JacobianConfig}
+  cfg::C
+  value::Vector{T}
+  jacob::Matrix{T}
+end
+
+function FixedScratchData(system::fp.System{T}) where T
+  cfg = fp.JacobianConfig(system)
+  n = length(system)
+  d = fp.nvariables(system)
+  value = zeros(T,n)
+  jacob = zeros(T,(n,d))
+  FixedScratchData(cfg,value,jacob)
+end
+
+"""
+Implementation of a tensor-valued multivariate polynomial basis
+using the functionality given by the package FixedPolynomials
+"""
+struct FixedPolynomialBasis{T,V,G}
+  system::fp.System{T}
+end
+
+function ScratchData(b::FixedPolynomialBasis)
+  FixedScratchData(b.system)
+end
+
+function evaluate!(
+  v::AbstractVector{V},
+  b::FixedPolynomialBasis{T,V},
+  x::AbstractVector{T},
+  cache::FixedScratchData{T}) where {T,V}
+
+  fp.evaluate!(cache.value,b.system,x,cache.cfg)
+  _fill_value!(v,cache.value)
+end
+
+function gradient!(
+  v::AbstractVector{G},
+  b::FixedPolynomialBasis{T,V,G},
+  x::AbstractVector{T},
+  cache::FixedScratchData{T}) where {T,V,G}
+
+  fp.jacobian!(cache.jacob,b.system,x,cache.cfg)
+  _fill_gradient!(v,cache.jacob,V)
+end
+
+function _fill_value!(
+  v::AbstractVector{V},
+  s::AbstractVector{T}) where {V,T}
+
+  m = mutable(V)
+  k = 1
+  for si in s
+    for j in eachindex(m)
+      m .= 0.0
+      m[j] = si
+      v[k] = m
+      k += 1
+    end
+  end
+end
+
+function _fill_gradient!(
+  v::AbstractVector{G},
+  s::AbstractMatrix{T},
+  ::Type{V}) where {T,V,G}
+
+  m = zero(mutable(G))
+  w = zero(V)
+  k = 1
+  for i in 1:size(S,1)
+    for l in CartesianIndices(w)
+      m .= 0.0
+      for j in 1:size(S,2)
+        m[j,l] = S[i,j]
+        v[k] = m
+      end
+      k += 1
+    end 
+  end
+end
+
+function gradient_type(::Type{A},::Val{D}) where A<:StaticArray{S,T,N} where {S,T,N,D}
+  SG = _gradient_size(Size(A),Val(D))
+  TG = T
+  NG = N+1
+  LG = _gradient_length(Size(A),Val(D))
+  SArray{SG,TG,NG,LG}
+end
+
+@generated function _gradient_size(::Size{B},::Val{D}) where {B,D}
+  str = join(["$b," for b in B])
+  Meta.parse("Tuple{$D,$str}")
+end
+
+function _gradient_length(::Size{B},::Val{D}) where {B,D}
+  prod((D,B...))
 end
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,20 +2,29 @@ module TensorPolynomialBasesTests
 
 using TensorPolynomialBases
 using Test
-using FixedPolynomials
+import FixedPolynomials; const fp = FixedPolynomials
+using StaticArrays
 
 filter(e,O) = (sum(e) <= O)
 
 order = 3
 dim = 2
-B = System{Float64}(filter,order,dim)
+B = fp.System{Float64}(filter,order,dim)
 
-cfg = JacobianConfig(B)
+cfg = fp.JacobianConfig(B)
 
 x = [1.0,2.0]
 
-@test evaluate(B,x,cfg) ≈ [1.0, 1.0, 1.0, 1.0, 2.0, 2.0, 2.0, 4.0, 4.0, 8.0]
-@test jacobian(B,x,cfg) ≈ [0.0 0.0; 1.0 0.0; 2.0 0.0; 3.0 0.0; 0.0 1.0;
+@test fp.evaluate(B,x,cfg) ≈ [1.0, 1.0, 1.0, 1.0, 2.0, 2.0, 2.0, 4.0, 4.0, 8.0]
+@test fp.jacobian(B,x,cfg) ≈ [0.0 0.0; 1.0 0.0; 2.0 0.0; 3.0 0.0; 0.0 1.0;
                      2.0 1.0; 4.0 1.0; 0.0 4.0; 4.0 4.0; 0.0 12.0]
+
+#order = 3
+#basis = FixedPolynomialBasis{T,SVector{Z,T}}(filter,order)
+
+@test gradient_type(SVector{3,Int},Val(2)) == SMatrix{2,3,Int,6}
+
+@test gradient_type(SMatrix{3,4,Int},Val(2)) == SArray{Tuple{2,3,4},Int,3,24}
+
 
 end # module TensorPolynomialBasesTests
