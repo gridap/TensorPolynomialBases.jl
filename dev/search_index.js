845157e6d26b909bc3a817f0d348447fbe934ba7
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-05-19 15:15:19 +0000

    build based on 43a4340

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#TensorPolynomialBases.jl-1","page":"TensorPolynomialBases.jl","title":"TensorPolynomialBases.jl","text":"","category":"section"},{"location":"#","page":"TensorPolynomialBases.jl","title":"TensorPolynomialBases.jl","text":"Documentation for TensorPolynomialBases.jl","category":"page"}]
+}
