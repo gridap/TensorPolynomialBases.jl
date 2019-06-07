588bc42154b8a570fc5f3e7cf06f18b7348407b4
fverdugo <fverdugo@cimne.upc.edu>
2019-06-07 07:38:00 +0200

    Updated reference to TensorValues

--- src/TensorPolynomialBases.jl
+++ src/TensorPolynomialBases.jl
@@ -1,46 +1,5 @@
 module TensorPolynomialBases
 
-### To be removed
-
-using TensorValues
-using StaticArrays
-import Base: convert
-import Base: CartesianIndices
-import Base: LinearIndices
-
-function convert(::Type{<:MultiValue{S,T,N,L}},a::StaticArray{S,T,N}) where {S,T,N,L}
-  MultiValue(a)
-end
-
-function CartesianIndices(a::MultiValue)
-  CartesianIndices(a.array)
-end
-
-function LinearIndices(a::MultiValue)
-  LinearIndices(a.array)
-end
-
-import Base: length
-length(::Type{MultiValue{S,T,N,L}}) where {S,T,N,L} = L
-
-import Base: isapprox
-function isapprox(
-  a::AbstractArray{<:MultiValue}, b::AbstractArray{<:MultiValue})
-  if size(a) != size(b); return false; end
-  for (ai,bi) in zip(a,b)
-    if !(ai≈bi); return false; end
-  end
-  true
-end
-
-function convert(
-  ::Type{<:MultiValue{S,T,N,L}},a::AbstractArray{T,N}) where {S,T,N,L}
-  b = convert(SArray{S,T,N,L},a)
-  MultiValue(b)
-end
-
-###
-
 using StaticArrays
 using TensorValues
 using Test
